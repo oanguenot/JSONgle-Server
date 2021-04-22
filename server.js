91e2347253e5d615c363f2356351990a4589e596
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2021-04-22 10:12:53 +0200

    Refactor and add certificates

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const path = require("path");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
 const { logInfo } = require("./modules/Logger");
 const morgan = require("morgan");
-const { urlencoded } = require("express");
+//const { urlencoded } = require("express");
+var fs = require('fs');
 
 const { generateNewId } = require("./modules/helper")
 
@@ -13,19 +11,20 @@ morgan(":method :url :status :res[content-length] - :response-time ms");
 
 const users = {};
 
+var options = {
+    key: fs.readFileSync('./certificates/server.key'),
+    cert: fs.readFileSync('./certificates/server.crt')
+};
+
+var app = require('https').createServer(options);
+var io = require('socket.io').listen(app);
+app.listen(8080);
+
+
 io.sockets.on("connection", (socket, pseudo) => {
     logInfo(`[io    ] new client connected ${socket.id}`);
 
-    // Send message about already connected users
-    // var sockets = io.sockets.sockets;
-    // Object.keys(sockets).forEach((id) => {
-    //     if (id !== socket.id) {
-    //         io.to(socket.id).emit("users", { action: "userDidConnect", data: { userId: id } });
-    //     }
-    // });
-
     socket.join("room");
-    // socket.to("room").emit("users", { action: "userDidConnect", data: { userId: socket.id } });
 
     socket.on("hello", (message) => {
 
@@ -41,19 +40,7 @@ io.sockets.on("connection", (socket, pseudo) => {
         io.to(users[message.to]).emit("welcome", message.data);
     });
 
-    socket.on("sig:offer", (message) => {
-        logInfo("[io    ] OFFER ", message);
-        socket.to("room").emit("sig:offer", message);
-    });
-
-    socket.on("sig:candidate", (message) => {
-        logInfo("[io    ] ICE ", message);
-        socket.to("room").emit("sig:candidate", message);
-    });
-
     socket.on("disconnect", () => {
-        console.log("DISCONNETED")
-
         let id = Object.keys(users).find((id) => {
             return (users[id] === socket.id);
         });
@@ -64,6 +51,28 @@ io.sockets.on("connection", (socket, pseudo) => {
 
     socket.on("jsongle", (message) => {
         logInfo("[io    ] MESSAGE ", message);
+
+        if (!(message.to in users)) {
+            const abortMsg = {
+                id: generateNewId(),
+                from: "server",
+                to: message.from,
+                jsongle: {
+                    sid: message.jsongle.sid,
+                    action: "session-info",
+                    reason: "unreachable",
+                    initiator: message.jsongle.initiator,
+                    responder: message.jsongle.responder,
+                    description: {
+                        ended: new Date().toJSON(),
+                    },
+                },
+            };
+
+            // Send a try to issuer
+            socket.emit("jsongle", abortMsg);
+            return;
+        }
 
         if (message.jsongle.action === "session-propose") {
 
@@ -78,9 +87,7 @@ io.sockets.on("connection", (socket, pseudo) => {
                     initiator: message.jsongle.initiator,
                     responder: message.jsongle.responder,
                     description: {
-                        initiated: message.jsongle.initiated,
-                        ended: message.jsongle.ended,
-                        ended_reason: message.jsongle.ended_reason,
+                        tried: new Date().toJSON(),
                         media: message.jsongle.media,
                         additional_data: {
                             initiator_name: "",
@@ -98,12 +105,4 @@ io.sockets.on("connection", (socket, pseudo) => {
         // Forward message to responder
         io.to(users[message.to]).emit("jsongle", message);
     });
-});
-
-// io.sockets.on("disconnect", () => {
-//     logInfo("[io    ] client connected");
-// });
-
-http.listen(8080, () => {
-    logInfo("[server] started on port 8080");
 });
