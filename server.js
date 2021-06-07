0cabf0529571190905a1be915db58d9828fd5883
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2021-06-07 20:34:37 +0200

    Do not try to start REST and WebSocket servers when no certificate files exist

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require('cors')
 
 const { CONFIG, configure } = require("./modules/services/config");
-const { info, debug, setLevelTo, getLogLevel, createLogger } = require("./modules/services/logger");
+const { info, debug, setLevelTo, getLogLevel, createLogger, error } = require("./modules/services/logger");
 const socket = require('./modules/services/socket');
 const { collect, requestCounters, responseCounters } = require("./modules/services/prom");
 
@@ -36,37 +36,41 @@ const initialize = () => {
     app.use(requestCounters);
     app.use(responseCounters);
 
-    const options = {
-        key: fs.readFileSync(String(CONFIG().key)),
-        cert: fs.readFileSync(String(CONFIG().cert))
-    };
+    if (fs.existsSync(String(CONFIG().key)) && fs.existsSync(String(CONFIG().cert))) {
+        const options = {
+            key: fs.readFileSync(String(CONFIG().key)),
+            cert: fs.readFileSync(String(CONFIG().cert))
+        };
 
-    debug({ module: moduleName, label: `setup REST API server on port ${CONFIG().restPort}` });
-    const restServer = require('https').createServer(options, app);
-    restServer.listen(8081, () => {
-        debug({ module: moduleName, label: `REST API server started successfully on port ${CONFIG().restPort}` });
-    });
+        debug({ module: moduleName, label: `setup REST API server on port ${CONFIG().restPort}` });
+        const restServer = require('https').createServer(options, app);
+        restServer.listen(8081, () => {
+            debug({ module: moduleName, label: `REST API server started successfully on port ${CONFIG().restPort}` });
+        });
 
-    debug({ module: moduleName, label: "setup routes for API server" });
-    require('./modules/routes/serviceability')(app);
-    require('./modules/routes/metrics')(app);
+        debug({ module: moduleName, label: "setup routes for API server" });
+        require('./modules/routes/serviceability')(app);
+        require('./modules/routes/metrics')(app);
 
-    debug({ module: moduleName, label: "start collecting metrics" });
-    collect();
+        debug({ module: moduleName, label: "start collecting metrics" });
+        collect();
 
-    debug({ module: moduleName, label: `setup webSockets server on port ${CONFIG().wsPort}` });
-    const wsServer = require('https').createServer(options);
-    const io = require('socket.io')(wsServer, {
-        cors: {
-            origin: CONFIG().corsPolicyOrigin,
-            methods: ["GET", "POST"],
-            credentials: true
-        }
-    });
-    socket.listen(io, CONFIG());
-    wsServer.listen(CONFIG().wsPort, () => {
-        debug({ module: moduleName, label: `webSockets server started successfully on port ${CONFIG().wsPort}` });
-    });
+        debug({ module: moduleName, label: `setup webSockets server on port ${CONFIG().wsPort}` });
+        const wsServer = require('https').createServer(options);
+        const io = require('socket.io')(wsServer, {
+            cors: {
+                origin: CONFIG().corsPolicyOrigin,
+                methods: ["GET", "POST"],
+                credentials: true
+            }
+        });
+        socket.listen(io, CONFIG());
+        wsServer.listen(CONFIG().wsPort, () => {
+            debug({ module: moduleName, label: `webSockets server started successfully on port ${CONFIG().wsPort}` });
+        });
+    } else {
+        error({ module: moduleName, label: `file ${String(CONFIG().key)} or ${String(CONFIG().cert)} is missing - can't start REST and Websocket servers` })
+    }
 
     info({ module: moduleName, label: `initialization done!` });
 }
